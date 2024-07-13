d3ce0abe89363896f48d7a28cc96e2390c3631ed
mac <am@trade45.ru>
2024-07-13 22:38:18 +0300

    add pages

---
+++ src/pages/auto/AutoPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    Гараж
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
---
+++ src/pages/history/HistoryPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    История заказов
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
